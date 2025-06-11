05103988962325b8a763a97fd2e94457066cabfc
Dmitry Litvinov <dimanlitvinoff@gmail.com>
2025-06-11 23:51:28 +0300

    some improvements

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,9 @@ FROM python:3.11-slim
 WORKDIR /app
 
 COPY requirements.txt .
+
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD ["pytest", "-sv", "--alluredir=allure-report", "--clean-alluredir"]
+CMD ["pytest", "-v"]
--- Tests/test_booking.py
+++ Tests/test_booking.py
@@ -1,6 +1,15 @@
 from services.api_service import APIService
+import allure
 
+@allure.epic('First test')
 class TestBooking:
+    @allure.title('First test')
     def test_get_booking(self):
-        booking = APIService().get_booking(10)
-        assert booking.status_code == 200
+        with allure.step('Отправка тестового запроса'):
+            booking = APIService().get_booking(10)
+
+        with allure.step('Первый assert'):
+            assert booking.status_code == 200
+
+        with allure.step('Ответ от сервера'):
+            allure.attach(body=booking.text, name='Server response', attachment_type=allure.attachment_type.TEXT)
